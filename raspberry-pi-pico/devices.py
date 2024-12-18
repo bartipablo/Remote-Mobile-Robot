fefb7aeb4ef576bfa915e6b5cfc067234ae29d28
bartipablo <bartek.pawlowski00@gmail.com>
2024-12-18 20:50:32 +0100

    battery lvl and led

--- raspberry-pi-pico/devices.py
+++ raspberry-pi-pico/devices.py
@@ -25,3 +25,12 @@ class Buzzer:
     def disable_if_time_expired(self, expiried_time):
         if self.is_enabled() and time.ticks_diff(time.ticks_ms(), self.buzzer_start_time) >= expiried_time:
             self.toggle_buzzer(False)
+
+
+class VoltageMeter:
+    def __init__(self, pin):
+        self.pin = pin
+
+    def read_voltage(self):
+        return (self.pin.read_u16() / 99997) * 25
+    
--- raspberry-pi-pico/main.py
+++ raspberry-pi-pico/main.py
@@ -1,17 +1,25 @@
 import time
-
-from machine import UART, Pin
-from devices import Buzzer
+import _thread
+ 
+from machine import UART, Pin, ADC
+from devices import Buzzer, VoltageMeter
 
 # HARDWARE CONFIGURATION
+LED = Pin(25, Pin.OUT)
+LED.value(1)
+
 uart1 = UART(0, baudrate=500000, tx=Pin(0), rx=Pin(1))
 
 pin_buzzer = Pin(15, Pin.OUT)
+
+pin_voltage_meter = ADC(Pin(26))
 # HARDWARE CONFIGURATION
 
 
 # SOFTWARE CONFIGURATION
 buzzer = Buzzer(pin_buzzer)
+
+voltage_meter = VoltageMeter(pin_voltage_meter)
 # SOFTWARE CONFIGURATION
 
 
@@ -24,6 +32,15 @@ def device_handling():
     buzzer.disable_if_time_expired(600)
 
 
+def transmit_read_voltage_task():
+    while True:
+        voltage = voltage_meter.read_voltage()
+        uart1.write(str(voltage) + "\n")
+        time.sleep(1)
+
+_thread.start_new_thread(transmit_read_voltage_task, ())
+
+# MAIN LOOP
 while True:
     message = uart1.readline()
 
